ba4db32e09cd85b0a4b5d7a896804f2537b02603
Valdir <valdir.silva@fcamara.com.br>
2021-05-08 07:06:14 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ProjetoPorcentagem';
+  porcentagem: number = 35;
+
+
+  Adcionar() {
+    if (this.porcentagem < 100) {
+      this.porcentagem += 5;
+    }
+  }
+
+
+  Remover() {
+    if (this.porcentagem > 0) {
+      this.porcentagem -= 5;
+    }
+  }
+
+
+
 }
